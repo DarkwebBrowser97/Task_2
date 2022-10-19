9b8f4b750d2797023fb271dc2c8f44ebe1082a6a
hristo <112704195+DarkwebBrowser97@users.noreply.github.com>
2022-10-19 11:33:31 +0300

    Add files via upload

--- TASK_2.01.java
+++ TASK_2.01.java
@@ -3,15 +3,25 @@ import java.util.Scanner;
 
 class TASK {
     public static void main(String[] args){
-        System.out.println("a_n = 3 + 2 * (n + 1)");
-        System.out.println("n = ");
+        System.out.println("Enter a value for N :");
         Scanner sc  = new Scanner(System.in);
         int n = Integer.parseInt(sc.nextLine());
+        System.out.println("a_n = 3 + 2 * (n + 1)");
+        System.out.println("n = " + n);
+        int sum = 0;
+        int  sumDividedByFive = 0;
         for (int i = 0; i <= n; i++)
         {
-            System.out.println("a_" + i);
             int a = 3 + 2*(i+1);
             System.out.println("a_" + i + "=" + a);
+            sum += a;
+            if(a % 5 == 0) {
+                sumDividedByFive += a;
+            }
         }
+
+        System.out.println("Sum is : " + sum);
+        System.out.println("Sum of number which are divided by five is : " + sumDividedByFive);
     }
+
 }
