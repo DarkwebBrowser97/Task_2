801c07e5164ee89e7eb0ef1843bd917195115d4c
hristo <112704195+DarkwebBrowser97@users.noreply.github.com>
2023-06-08 19:11:28 +0300

    Add files via upload

---
+++ Spammmbot.java
@@ -0,0 +1,46 @@
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+import java.util.Scanner;
+
+public class Spammmbot {
+
+    public static void main (String[]args) throws AWTException, InterruptedException {
+        int  x =0;
+        Robot robot = new Robot();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your Text:");
+        String Text = scanner.nextLine();
+
+        StringSelection  stringSelection = new StringSelection(Text);
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(stringSelection,null);
+
+        System.out.println("The spamming  will begin in 5 seconds");
+        Thread.sleep(5000);
+
+        while(x<=30){
+
+            Thread.sleep(1000);
+
+            robot.keyPress(KeyEvent.VK_CONTROL);
+            robot.keyPress(KeyEvent.VK_V);
+            robot.keyPress(KeyEvent.VK_CONTROL);
+            robot.keyPress(KeyEvent.VK_V);
+
+            robot.keyPress(KeyEvent.VK_ENTER);
+            robot.keyPress(KeyEvent.VK_ENTER);
+
+
+
+
+
+
+            x++;
+
+
+        }
+    }
+}
