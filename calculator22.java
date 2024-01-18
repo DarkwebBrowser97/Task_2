e0611d8e67d28784c5a6a6648368dd9ca98cd8ae
hristo <112704195+DarkwebBrowser97@users.noreply.github.com>
2024-01-18 08:57:31 +0200

    Add files via upload

---
+++ calculator22.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class calculator22 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the operation (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        double result = 0;
+
+        if (operator == '+') {
+            result = num1 + num2;
+        } else if (operator == '-') {
+            result = num1 - num2;
+        } else if (operator == '*') {
+            result = num1 * num2;
+        } else if (operator == '/') {
+            if (num2 != 0) {
+                result = num1 / num2;
+            } else {
+                System.out.println("Error: Cannot divide by zero.");
+                System.exit(0);
+            }
+        } else {
+            System.out.println("Error: Invalid operator.");
+            System.exit(0);
+        }
+
+        System.out.println("Result: " + result);
+    }
+}
